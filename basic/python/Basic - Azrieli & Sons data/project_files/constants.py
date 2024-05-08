10ee851ad2f896aea408e9bfde7365170082456b
Ron Tohar <rontoh@post.bgu.ac.il>
2024-05-08 18:46:03 +0300

    save work

--- basic/python/Basic - Azrieli & Sons data/project_files/constants.py
+++ basic/python/Basic - Azrieli & Sons data/project_files/constants.py
@@ -1,4 +1,5 @@
 MALE = "m"
 FEMALE = "f"
+UNKOWN = "u"
 trips_folder = "../files/trips_data"
 basic_milage_limit = 200
--- basic/python/Basic - Azrieli & Sons data/project_files/trip_file.py
+++ basic/python/Basic - Azrieli & Sons data/project_files/trip_file.py
@@ -6,16 +6,22 @@ class TripFile():
 
     def __init__(self, trip_file_path):
         self.trip_file_path = trip_file_path
-        self.trip_df = pd.read_csv(trip_file_path, index_col=0)
+        self.trips_df = pd.read_csv(trip_file_path, index_col=0)
         self.file_name = os.path.basename(trip_file_path)
         self.month = self.file_name.split(' ')[0]
         self.year = self.file_name.split(' ')[1].split('_')[0]
 
+        self.preprocess_trips_df(self.trips_df)
+
     def get_file_name(self):
         return self.file_name
+    
+    def preprocess_trips_df(self, trips_df):
+        trips_df.start_time = pd.to_datetime(trips_df.start_time)
+        trips_df.end_time = pd.to_datetime(trips_df.end_time)
 
     def get_data_frame(self):
-        return self.trip_df
+        return self.trips_df
 
     def get_month(self):
         return self.month
